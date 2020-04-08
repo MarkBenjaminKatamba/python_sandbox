2e8d9151c207d0c52a4a208efe7903a07b22bd8b
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-04-08 15:36:05 +0300

    Looked at Variables, Strings and Lists

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,63 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Crystal'
+age = 21
+
+#Concatenate (basically means to insert a variable into a string)
+#print ('Hello, my name is ' + name + ' and I am ' + str(age))
+
 
 # String Formatting
+#Arguments by position/Positional arguments
+#print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+#Using F-stirngs (these are only available in Python 3.6+)
+#print(f'Hello, my name is {name} and I am {age}')
+
 
 # String Methods
+s = 'hello world'
+
+#Capitalize
+print(s.capitalize())       #Capitalizes the first letter of the string
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lowercase
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get lenght (gets the length of the string)
+print(len(s)) 
+
+# Replace
+print(s.replace('world', 'Claire'))
+
+# Count
+#print(s.count('h'))
+sub = 'h'
+print(s.count(sub))
+
+#Starts with
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('d'))
+
+# Split into a list
+print(s.split())
+
+# Find Position
+print(s.find('r'))
+
+# Is all alphanumeric
+print(s.isalnum())
+
+# Is all alphabetic
+print(s.isalpha())
+
+# Is all numeric
+print(s.isnumeric())
--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,23 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# x = 1               # int
+# y = 2.5             # float
+# name = 'Claire'     # str
+# is_cool = True      # bool
+
+#Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Claire', True)      #Same as above implementation, just that this is more compact.
+
+#Basic math
+a = x + y
+
+#Casting (Here, we're able to change the type of an integer value)
+
+x = str(x)          #Changoing the type of x from 'int' to 'string'
+y = int(y)          #Changoing the type of y from 'float' to 'int' so it'll print out as 2 instead of 2.5
+z = float(y)        #Changoing the type of x from 'int' back to 'float', so now it'll print out as 2.0
+
+#print (x, y, name, is_cool, a)
+print (type(y), y)
