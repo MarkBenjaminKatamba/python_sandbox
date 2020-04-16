26c1c964ec6f7395d465b71392c592254a732a6a
root <root@localhost.localdomain>
2020-04-15 21:49:20 -0400

    Looked at Functions and Conditionals

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,19 +1,72 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
-
-
+c = 16
+d = 16.4
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
+
+# Simple if
+# if c > d:
+#     print(f'{c} is greater than {d}')
+
+# If/else
+# if c > d:
+#     print(f'{c} is greater than {d}')
+# else:
+#     print(f'{d} is greater than {c}')
+
+# elif
+# if c > d:
+#     print(f'{c} is greater than {d}')
+# elif c==d:
+#     print(f'{c} is equal to {d}') 
+# else:
+#     print(f'{d} is greater than {c}')
+
+# Nested if  (Not always recommended; better to use logcal operators)
+# if c > 2:
+#     if c <= 10:
+#         print(f'{c} is greater than 2 and less than or equal to 10')
 
 
 
 # Logical operators (and, or, not) - Used to combine conditional statements
+
+# # and (here, both conditions have to be true)
+# if c > 2 and c <= 10:
+#     print(f'{c} is greater than 2 and less than or equal to 10')
+    
+
+# # or (here, either one or the other of the conditions should be true)
+# if c > 2 or c <= 10:
+#     print(f'{c} is greater than 2 or less than or equal to 10')
+
+# # not
+# if not(c == d):
+#     print(f'{c} is not equal to {d}')
 
 
 
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
 
+numbers = [1,2,3,4,5,6,7,8,9]
+
+# #  in
+# if c in numbers:
+#     print(c in numbers) # This will just give us a true of false
+
+# #  not in
+# if c not in numbers:
+#     print(c not in numbers)
 
 
 
 # Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+# # is
+# if c is d:
+#     print(c is d)
+
+# # is not
+# if c is not d:
+#     print(c is not d)
--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -6,22 +6,23 @@
 def sayHello(name):
         print('Hello ' + name + '!')
 
-sayHello('Crystal April')
+#sayHello('Crystal April')
+
 
 # Return Vlaues (Commonest use of functions, coz for the most part of 
 # it you'll never simply print out stuff like you just did above)
 def getSum(num1, num2):
-    total = num1 + num2
-    return total
+        total = num1 + num2
+        return total
 
-# num = getSum(5,6)
+# num = getSum(4.2, 7)
 # print(num)
+# print(getSum(3.5, 5))
 
 
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, # but can only have one expression. 
 # Very similar to JS arrow functions
-getSum = lambda num1, num2: num1 + num2
-print(getSum(12,4))
-
+getSum = lambda num1, num2 : num1 + num2   #So here, the colon acts like the arrow in an arrow finction. "num1 + num2" is the body of the function, and it's going to implicitly return whatever the "num1 + num2" expression equals and it's going to put it in the getSum variable.
+print(getSum(12, 18))
